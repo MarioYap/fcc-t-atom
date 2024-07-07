cc667a54d7da7e79feae9ff177370c6c6e08dbd6
Mario Dharma <dharmamario57@gmail.com>
2024-07-07 07:18:05 +0000

    fix: fixed dbname in PSQL var and added piping

--- element.sh
+++ element.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+ELEMENT_Q=$($PSQL "SELECT * FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id)")
 
-echo "$($PSQL= "SELECT * FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id)")" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELT_POINT BAR BOIL_POINT
+echo "$ELEMENT_Q" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELT_POINT BAR BOIL_POINT
 do 
   # commands here
+  echo "$ATOMIC_NUMBER $NAME"
 done
