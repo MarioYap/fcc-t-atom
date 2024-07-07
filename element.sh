1e0ea6ae3959cde4f7bdaf7d1748cc0b31dfced6
Mario Dharma <dharmamario57@gmail.com>
2024-07-07 08:36:06 +0000

    test: adjusted piping codes

--- element.sh
+++ element.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-ELEMENT_Q=$($PSQL "SELECT * FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id)")
+ELEMENT_Q=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id)")
 
-echo "$ELEMENT_Q" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELT_POINT BAR BOIL_POINT
-do 
-  # commands here
-  echo "$ATOMIC_NUMBER $NAME"
-done
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  echo "$ELEMENT_Q" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELT_POINT BOIL_POINT TYPE
+  do
+    echo "$ATOMIC_NUMBER $NAME $TYPE"
+  done
+fi
