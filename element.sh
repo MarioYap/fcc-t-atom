9e4f05bdbf15b9661aa1fee879fc4d35b14632ff
Mario Dharma <dharmamario57@gmail.com>
2024-07-07 06:53:30 +0000

    feat: value piping

--- element.sh
+++ element.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
+echo "$($PSQL= "SELECT * FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id)")" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELT_POINT BAR BOIL_POINT
+do 
+  # commands here
+done
